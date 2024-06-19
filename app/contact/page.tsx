4454f26eca2081c615c76712562b7595f1d79c78
siwakorn-chantawat <siwakorn.chantawat@gmail.com>
2024-06-19 13:07:09 +0700

    ✅ Add all pages and routing

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import GetInTouch from "@/components/GetInTouch";
+import Link from "next/link";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="mt-auto">
+      <GetInTouch />;
+    </div>
+  );
+};
+
+export default Contact;
---
+++ app/form/page.tsx
@@ -0,0 +1,14 @@
+import MultiStepForm from "@/components/MultiStepForm";
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-full max-w-4xl min-h-3/4">
+        <MultiStepForm />
+      </div>
+    </div>
+  );
+};
+
+export default Form;
